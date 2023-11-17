b44640cc66a4059a5f1df92d20d23e9c07a10181
kshoon99 <kshun08@dongguk.edu>
2023-11-17 21:12:16 +0900

    feat: 로그인 api 연동

--- src/lib/customAxios.ts
+++ src/lib/customAxios.ts
@@ -5,11 +5,9 @@ const host =
     ? import.meta.env.VITE_API_URL
     : "api";
 
-// import cookies from "js-cookie";
 export const customAxios: AxiosInstance = axios.create({
   baseURL: host, // 기본 서버 주소 입력
-  // headers: {},
   //   headers: {
-  //     access_token: cookies.get("access_token"),
+  //     user_id: cookies.get("access_token"),
   //   },
 });
---
+++ src/utils/storage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { UserData } from "../types/UserData";
+
+const storage = {
+  set: (key: string, value: UserData) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  get: (key: string): UserData => {
+    const defaultValue = {
+      userId: -1,
+    };
+    const value = localStorage.getItem(key);
+    return (value ? JSON.parse(value) : defaultValue) as UserData;
+  },
+  remove: (key: string) => {
+    localStorage.removeItem(key);
+  },
+};
+
+export default storage;
