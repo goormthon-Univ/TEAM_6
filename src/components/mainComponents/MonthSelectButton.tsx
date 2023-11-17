86baebdb0973064d9a549fd2aecbf1d012082443
Jiwon <58460386+isdiscodead@users.noreply.github.com>
2023-11-18 02:46:29 +0900

    add plan end

--- src/components/mainComponents/MonthSelectButton.tsx
+++ src/components/mainComponents/MonthSelectButton.tsx
@@ -8,19 +8,33 @@ type Props = {
 
 function MonthSelectButton({month}: Props) {
   return (
-    <Button>{month}개월</Button>
+     <Button>
+      <Radio name='period' type='radio' value={month} id={'radio'+month}/>
+      <Label htmlFor={'radio'+month}>{month}개월</Label>
+    </Button>
   )
 }
 
-const Button = styled.button`
+const Button = styled.div``;
+
+const Radio = styled.input`
+  display: none;
+`;
+
+const Label = styled.label`
   display: flex;
   border: none;
   border-radius: 1rem;
   width: 92px;
+  background: var(--ion-color-secondary);
   height: 48px;
   justify-content: center;
   align-items: center;
   font-weight: bold;
   color: var(--ion-color-dark);
-`
+  ${Radio}:checked + & {
+    background: var(--ion-color-primary);
+  }
+`;
+
 export default MonthSelectButton
